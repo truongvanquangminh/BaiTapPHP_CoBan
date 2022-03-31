e39b6eef631340c1047eac6fd3ae9fa81e09024d
Quang Minh <minhtruong20002@gmail.com>
2022-03-31 09:24:52 +0700

    Add Bai 5,7,8,9,10

--- Bai5.php
+++ Bai5.php
@@ -1,1 +1,16 @@
 <!-- Cho một chuỗi gồm nhiều số, tìm số bé nhất trong chuỗi. -->
+<?php
+
+$string = '2461';
+$n = str_split($string);
+$min = null;
+for ($i = 0; $i < count($n); $i++) {
+    if ($min == null) {
+        $min = $string[0];
+    } else {
+        if ($n[$i] < $min) {
+            $min = $n[$i];
+        }
+    }
+}
+echo "Số nhỏ nhất là: " . $min;
